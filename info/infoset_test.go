e107dd73c5b32a675543940a502c862af679a030
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-01-21 21:26:31 -0300

    chore: add Infosets tests

---
+++ info/infoset_test.go
@@ -0,0 +1,278 @@
+package info
+
+import (
+	"testing"
+
+	"github.com/filevich/truco-cfr/abs"
+	"github.com/truquito/truco/pdt"
+)
+
+// p, _ := pdt.NuevaPartida(pdt.A20, []string{"Alice", "Anna"}, []string{"Bob", "Ben"}, 1, true)
+// bs, _ := p.MarshalJSON()
+
+var (
+	verbose = true
+)
+
+func TestInfosetRondaBase(t *testing.T) {
+	p, _ := pdt.Parse(`{"puntuacion":20,"puntajes":{"azul":0,"rojo":0},"ronda":{"manoEnJuego":0,"cantJugadoresEnJuego":{"azul":2,"rojo":2},"elMano":0,"turno":0,"envite":{"estado":"noCantadoAun","puntaje":0,"cantadoPor":"","sinCantar":["Ben"]},"truco":{"cantadoPor":"","estado":"noGritadoAun"},"manojos":[{"seFueAlMazo":false,"cartas":[{"palo":"espada","valor":5},{"palo":"basto","valor":1},{"palo":"oro","valor":7}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Alice","equipo":"azul"}},{"seFueAlMazo":false,"cartas":[{"palo":"espada","valor":1},{"palo":"oro","valor":10},{"palo":"copa","valor":10}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Bob","equipo":"rojo"}},{"seFueAlMazo":false,"cartas":[{"palo":"basto","valor":10},{"palo":"basto","valor":7},{"palo":"copa","valor":7}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Anna","equipo":"azul"}},{"seFueAlMazo":false,"cartas":[{"palo":"copa","valor":6},{"palo":"copa","valor":3},{"palo":"copa","valor":12}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Ben","equipo":"rojo"}}],"mixs":{"Alice":0,"Anna":2,"Ben":3,"Bob":1},"muestra":{"palo":"espada","valor":6},"manos":[{"resultado":"indeterminado","ganador":"","cartasTiradas":[]},{"resultado":"indeterminado","ganador":"","cartasTiradas":[]},{"resultado":"indeterminado","ganador":"","cartasTiradas":[]}]},"limiteEnvido":1}`, verbose)
+	t.Log(p)
+	a := abs.A1{}
+	c := pdt.GetA(p, p.Manojo("Anna"))
+	i := MkInfoset1(p, p.Manojo("Anna"), c, a)
+	t.Log(i.Dump(false))
+}
+
+func eq[T comparable](s1, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestInfosetRondaLarge1NullAbs(t *testing.T) {
+	p, _ := pdt.Parse(`{"puntuacion":20,"puntajes":{"azul":0,"rojo":0},"ronda":{"manoEnJuego":0,"cantJugadoresEnJuego":{"azul":2,"rojo":2},"elMano":0,"turno":0,"envite":{"estado":"noCantadoAun","puntaje":0,"cantadoPor":"","sinCantar":["Ben"]},"truco":{"cantadoPor":"","estado":"noGritadoAun"},"manojos":[{"seFueAlMazo":false,"cartas":[{"palo":"espada","valor":5},{"palo":"basto","valor":1},{"palo":"oro","valor":7}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Alice","equipo":"azul"}},{"seFueAlMazo":false,"cartas":[{"palo":"espada","valor":1},{"palo":"oro","valor":10},{"palo":"copa","valor":10}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Bob","equipo":"rojo"}},{"seFueAlMazo":false,"cartas":[{"palo":"basto","valor":10},{"palo":"basto","valor":7},{"palo":"copa","valor":7}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Anna","equipo":"azul"}},{"seFueAlMazo":false,"cartas":[{"palo":"copa","valor":6},{"palo":"copa","valor":3},{"palo":"copa","valor":12}],"tiradas":[false,false,false],"ultimaTirada":-1,"jugador":{"id":"Ben","equipo":"rojo"}}],"mixs":{"Alice":0,"Anna":2,"Ben":3,"Bob":1},"muestra":{"palo":"espada","valor":6},"manos":[{"resultado":"indeterminado","ganador":"","cartasTiradas":[]},{"resultado":"indeterminado","ganador":"","cartasTiradas":[]},{"resultado":"indeterminado","ganador":"","cartasTiradas":[]}]},"limiteEnvido":1}`, verbose)
+	t.Log(p)
+	a := abs.Null{}
+
+	// para Anna
+	{
+		i := NewInfosetRondaLarge(p, p.Manojo("Anna"), a, nil)
+		// t.Log(i.Dump(true))
+
+		irl, _ := i.(*InfosetRondaLarge)
+
+		// 1. `muestra`
+		if ok := irl.muestra == 25; !ok {
+			t.Error()
+		}
+
+		// 2. `num_mano_actual`: int
+		if ok := irl.numMano == 0; !ok {
+			t.Error()
+		}
+
+		// 3. `rixMe` ~ RIX: who?
+		if ok := irl.rixMe == 2; !ok {
+			t.Error()
+		}
+
+		// 4. `turno` ~ RIX who?
+		if ok := irl.rixTurno == 0; !ok {
+			t.Error()
+		}
+
+		// 5. `ManojosEnJuego` quiénes se fueron al mazo y quiénes siguen en pie?
+		{
+			exp := []bool{true, true, true, true}
+			if ok := eq(irl.manojosEnJuego, exp); !ok {
+				t.Error()
+			}
+		}
+
+		// 6. `nuestrasCartas` representa nuestras cartas.
+		{
+			// cartas alice
+			// (1,basto;5,espada;7,oro) --> (00;24;36) --> (2*97*157) --> 30458
+			// (10,basto;7,basto;7,copa) --> (07;06;16) --> (19*17*59) --> 19057
+			exp := []int{30458, 19057}
+			if ok := eq(irl.nuestrasCartas, exp); !ok {
+				t.Error()
+			}
+		}
+
+		// mi manojo
+		if ok := irl._miManojoPID == 19057; !ok {
+			t.Error()
+		}
+
+		// 7. tiradas
+		{
+			exp := [][]int{{}, {}}
+			for i, e := range exp {
+				if ok := eq(irl.tiradasCartas[i], e); !ok {
+					t.Error()
+				}
+			}
+		}
+
+		{
+			exp := [][]int{{}, {}}
+			for i, e := range exp {
+				if ok := eq(irl.tiradasWho[i], e); !ok {
+					t.Error()
+				}
+			}
+		}
+
+		// 8. historial
+		{
+			expQuien := []int{}
+			expQue := []string{}
+			expCuanto := []int{}
+			for i := range expQue {
+				if ok := expQue[i] == irl.historialQue[i]; !ok {
+					t.Error()
+				}
+				if ok := expQuien[i] == irl.historialQuien[i]; !ok {
+					t.Error()
+				}
+				if ok := expCuanto[i] == irl.historialCuanto[i]; !ok {
+					t.Error()
+				}
+			}
+		}
+
+		// 9. chi
+		if ok := irl.ChiLen() == 1; !ok {
+			t.Error()
+		}
+	}
+
+	// para Alice
+	{
+		i := NewInfosetRondaLarge(p, p.Manojo("Alice"), a, nil)
+		irl, _ := i.(*InfosetRondaLarge)
+
+		// 3. `rixMe` ~ RIX: who?
+		if ok := irl.rixMe == 0; !ok {
+			t.Error()
+		}
+
+		// 9. chi: 3C + E|RE|FE + T + M = 8
+		if ok := irl.ChiLen() == 8; !ok {
+			t.Error()
+		}
+	}
+
+	// para Bob
+	{
+		i := NewInfosetRondaLarge(p, p.Manojo("Bob"), a, nil)
+		// t.Log(i.Dump(true))
+
+		irl, _ := i.(*InfosetRondaLarge)
+
+		// 1. `muestra`
+		if ok := irl.muestra == 25; !ok {
+			t.Error()
+		}
+
+		// 2. `num_mano_actual`: int
+		if ok := irl.numMano == 0; !ok {
+			t.Error()
+		}
+
+		// 3. `rixMe` ~ RIX: who?
+		if ok := irl.rixMe == 1; !ok {
+			t.Error()
+		}
+
+		// 4. `turno` ~ RIX who?
+		if ok := irl.rixTurno == 0; !ok {
+			t.Error()
+		}
+
+		// 5. `ManojosEnJuego` quiénes se fueron al mazo y quiénes siguen en pie?
+		{
+			exp := []bool{true, true, true, true}
+			if ok := eq(irl.manojosEnJuego, exp); !ok {
+				t.Error()
+			}
+		}
+
+		// 6. `nuestrasCartas` representa nuestras cartas.
+		{
+			// (1,esp;10,oro;10,copa) --> (20;37;17) --> (61*73*163) --> 725839
+			// (6,copa;3,copa;12,copa) --> (15;12;19) --> (41*53*71) --> 154283
+			exp := []int{725839, 154283}
+			if ok := eq(irl.nuestrasCartas, exp); !ok {
+				t.Error()
+			}
+		}
+
+		// mi manojo
+		if ok := irl._miManojoPID == 725839; !ok {
+			t.Error()
+		}
+
+		// 7. tiradas
+		{
+			exp := [][]int{{}, {}}
+			for i, e := range exp {
+				if ok := eq(irl.tiradasCartas[i], e); !ok {
+					t.Error()
+				}
+			}
+		}
+
+		{
+			exp := [][]int{{}, {}}
+			for i, e := range exp {
+				if ok := eq(irl.tiradasWho[i], e); !ok {
+					t.Error()
+				}
+			}
+		}
+
+		// 8. historial
+		{
+			expQuien := []int{}
+			expQue := []string{}
+			expCuanto := []int{}
+			for i := range expQue {
+				if ok := expQue[i] == irl.historialQue[i]; !ok {
+					t.Error()
+				}
+				if ok := expQuien[i] == irl.historialQuien[i]; !ok {
+					t.Error()
+				}
+				if ok := expCuanto[i] == irl.historialCuanto[i]; !ok {
+					t.Error()
+				}
+			}
+		}
+
+		// 9. chi
+		if ok := irl.ChiLen() == 1; !ok {
+			t.Error()
+		}
+	}
+
+	// para Ben
+	{
+		i := NewInfosetRondaLarge(p, p.Manojo("Ben"), a, nil)
+		// t.Log(i.Dump(true))
+
+		irl, _ := i.(*InfosetRondaLarge)
+
+		// 3. `rixMe` ~ RIX: who?
+		if ok := irl.rixMe == 3; !ok {
+			t.Error()
+		}
+
+		// 6. `nuestrasCartas` representa nuestras cartas.
+		{
+			// (1,esp;10,oro;10,copa) --> (20;37;17) --> (61*73*163) --> 725839
+			// (6,copa;3,copa;12,copa) --> (15;12;19) --> (41*53*71) --> 154283
+			exp := []int{725839, 154283}
+			if ok := eq(irl.nuestrasCartas, exp); !ok {
+				t.Error()
+			}
+		}
+
+		// mi manojo
+		if ok := irl._miManojoPID == 154283; !ok {
+			t.Error()
+		}
+
+		// 9. chi: M+F
+		if ok := irl.ChiLen() == 2; !ok {
+			t.Error()
+		}
+	}
+}
