199c7c3f55f77b1207c8ab2962405587865b1215
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-02-03 19:06:41 -0300

    ft: add flags

--- cmd/train/main.go
+++ cmd/train/main.go
@@ -15,13 +15,20 @@ import (
 
 // flags
 var (
-	modelPtr      = flag.String("model", "", "Filepath to .model file to continue training")
+	modelPtr      = flag.String("model", "", "Filepath to .model file to continue training from")
 	numPlayersPtr = flag.Int("p", 2, "Number of players")
 	trainerPtr    = flag.String("trainer", "es-vmccfr", "CFR variant")
 	absPtr        = flag.String("abs", "a1", "Abstraction")
 	threadsPtr    = flag.Int("threads", 1, "Threads")
 	saveDirPtr    = flag.String("dir", "/tmp", "Save directory")
-	tinyEvalPtr   = flag.Int("eval", 1_000, "Progress eval length.")
+	tinyEvalPtr   = flag.Int("eval", 1_000, "Progress eval length")
+	runPtr        = flag.String("run", "30m", "Total run time")
+	prunningPtr   = flag.String("prunning", "", "Start prunning after")
+	saveEveryPtr  = flag.String("saveEvery", "10m", "Saving interval")
+	evalEveryPtr  = flag.String("evalEvery", "1m", "Eval interval")
+	silentPtr     = flag.Bool("silent", true, "Silent model")
+	prefixPtr     = flag.String("prefix", "final_", "Model prefix")
+	resetPtr      = flag.Bool("reset", false, "Reset strategy sum")
 )
 
 func init() {
@@ -36,19 +43,53 @@ func init() {
 	log.Println("threads", *threadsPtr)
 	log.Println("saveDir", *saveDirPtr)
 	log.Println("tinyEval", *tinyEvalPtr)
+	log.Println("run", *runPtr)
+	log.Println("saveEvery", *saveEveryPtr)
+	log.Println("evalEvery", *evalEveryPtr)
+	log.Println("silent", *silentPtr)
+	log.Println("prefix", *prefixPtr)
+	log.Println("reset", *resetPtr)
 }
 
 func main() {
 
 	var (
-		saveDir     = *saveDirPtr
-		threads     = *threadsPtr
-		numPlayers  = *numPlayersPtr
-		algo        = *trainerPtr
-		tinyEval    = *tinyEvalPtr
-		model       = *modelPtr
-		abstraction = abs.ParseAbs(*absPtr)
+		saveDir          = *saveDirPtr
+		threads          = *threadsPtr
+		numPlayers       = *numPlayersPtr
+		algo             = *trainerPtr
+		tinyEval         = *tinyEvalPtr
+		model            = *modelPtr
+		abstraction      = abs.ParseAbs(*absPtr)
+		totalRunningTime time.Duration
+		prunningTreshold time.Duration
+		saveEvery        time.Duration
+		evalEvery        time.Duration
+		err              error
 	)
+
+	if totalRunningTime, err = time.ParseDuration(*runPtr); err != nil {
+		panic(err)
+	}
+
+	if *prunningPtr == "" {
+		prunningTreshold = cfr.NEVER
+		log.Println("prunning", "never")
+	} else {
+		prunningTreshold, err = time.ParseDuration(*prunningPtr)
+		log.Println("prunning", prunningTreshold)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if saveEvery, err = time.ParseDuration(*saveEveryPtr); err != nil {
+		panic(err)
+	}
+
+	if evalEvery, err = time.ParseDuration(*evalEveryPtr); err != nil {
+		panic(err)
+	}
 
 	var trainer cfr.ITrainer
 
@@ -84,66 +125,29 @@ func main() {
 		log.Println(eval.Fmt(rr, agents))
 	}
 
-	// trainer.Train(
-	// 	&cfr.ProfileTime{
-	// 		TotalRunningTime:  24 * time.Hour,
-	// 		Prunning_treshold: cfr.NEVER,
-	// 		// multi
-	// 		Threads: threads,
-	// 		Mu:      &sync.Mutex{},
-	// 		// io
-	// 		Save_every:  25 * time.Hour,
-	// 		Silent:      true,
-	// 		Save_dir:    save_dir,
-	// 		Save_prefix: "pre_",
-	// 		// tiny eval
-	// 		PostSave: post_save,
-	// 		// GC
-	// 		GC_every: 1 * time.Hour,
-	// 	},
-	// )
-
-	// log.Printf("Resetting strategy sums")
-	// trainer.Reset()
+	if *resetPtr {
+		log.Printf("Resetting strategy sums")
+		trainer.Reset()
+	}
 
 	trainer.Train(
 		&cfr.ProfileTime{
-			TotalRunningTime: 25 * time.Minute,
-			PrunningTreshold: cfr.NEVER,
+			TotalRunningTime: totalRunningTime,
+			PrunningTreshold: prunningTreshold,
 			// multi
 			Threads: threads,
 			Mu:      &sync.Mutex{},
 			// io
-			SaveEvery:  2 * time.Minute,
-			Silent:     true,
+			SaveEvery:  saveEvery,
+			Silent:     *silentPtr,
 			SaveDir:    saveDir,
-			SavePrefix: "final_",
+			SavePrefix: *prefixPtr,
 			PostSave:   nil,
 			// tiny eval
-			EvalEvery: 1 * time.Minute,
+			EvalEvery: evalEvery,
 			Evaluator: evaluator,
 			// GC
 			GCEvery: 100 * time.Hour,
 		},
 	)
-
-	// trainer.Train(
-	// 	&cfr.ProfileTime{
-	// 		TotalRunningTime:  4 * 24 * time.Hour,
-	// 		Prunning_treshold: time.Nanosecond,
-	// 		// multi
-	// 		Threads: threads,
-	// 		Mu:      &sync.Mutex{},
-	// 		// io
-	// 		Save_every:  24 * time.Hour,
-	// 		Silent:      true,
-	// 		Save_dir:    save_dir,
-	// 		Save_prefix: "final_",
-	// 		// tiny eval
-	// 		PostSave: post_save,
-	// 		// GC
-	// 		GC_every: 1 * time.Hour,
-	// 	},
-	// )
-
 }
