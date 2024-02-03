116a43ba84920a162b667f5b300adf375c943bb8
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-02-03 15:27:02 -0300

    ref: var names

--- eval2/tournament.go
+++ eval2/tournament.go
@@ -18,29 +18,29 @@ type Tournament struct {
 	Table          Table
 }
 
-func (t *Tournament) Start(ds dataset.Dataset, verbose bool) {
+func (tournament *Tournament) Start(ds dataset.Dataset, verbose bool) {
 
-	t.NumDoubleGames = len(ds)
+	tournament.NumDoubleGames = len(ds)
 
 	if verbose {
-		log.Printf("Tournament %dp:\n", t.NumPlayers)
-		for ix, agent := range t.Agents {
+		log.Printf("Tournament %dp:\n", tournament.NumPlayers)
+		for ix, agent := range tournament.Agents {
 			log.Printf("\t%2d. %s\n", ix+1, agent.UID())
 		}
 	}
 
-	for i := 0; i < len(t.Agents)-1; i++ {
-		agent1 := t.Agents[i]
-		ops := t.Agents[i+1:]
+	for i := 0; i < len(tournament.Agents)-1; i++ {
+		agent1 := tournament.Agents[i]
+		ops := tournament.Agents[i+1:]
 
 		ress := PlayMultipleDoubleGames(
 			agent1,
 			ops,
-			t.NumPlayers,
+			tournament.NumPlayers,
 			ds)
 
 		for j := 0; j < len(ops); j++ {
-			t.Table.Add(
+			tournament.Table.Add(
 				agent1.UID(),
 				ops[j].UID(),
 				ress[j],
@@ -49,7 +49,7 @@ func (t *Tournament) Start(ds dataset.Dataset, verbose bool) {
 
 		if verbose {
 			log.Printf("%s", agent1.UID())
-			if i == len(t.Agents)-2 {
+			if i == len(tournament.Agents)-2 {
 				log.Printf("\n\n")
 			} else {
 				log.Printf(", ")
@@ -63,41 +63,41 @@ func (t *Tournament) Start(ds dataset.Dataset, verbose bool) {
 	}
 }
 
-func (t *Tournament) Inscriptos(subtitle string) []interface{} {
+func (tournament *Tournament) Inscriptos(subtitle string) []interface{} {
 	// "0.857   8.437" ~> tiene len=20
 	res := make([]interface{}, 0)
-	for _, agent := range t.Agents {
+	for _, agent := range tournament.Agents {
 		res = append(res, fmt.Sprintf("%s\n  %s ", agent.UID(), subtitle))
 	}
 	return res
 }
 
-func (torneo *Tournament) PrintWrTable(tabla Table) {
+func (tournament *Tournament) PrintWrTable(tabla Table) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(
 		append(
 			table.Row{"A\\B"},
 			append(
-				torneo.Inscriptos("WP    ADP"),
+				tournament.Inscriptos("WP    ADP"),
 				"B/A",
 			)...,
 		),
 	)
 
-	for i := 0; i < len(torneo.Agents); i++ {
+	for i := 0; i < len(tournament.Agents); i++ {
 
-		agent1 := torneo.Agents[i]
+		agent1 := tournament.Agents[i]
 		row := []interface{}{
 			agent1.UID(),
 		}
 
-		for j := 0; j < len(torneo.Agents); j++ {
+		for j := 0; j < len(tournament.Agents); j++ {
 			if i == j {
 				row = append(row, "             ")
 				continue
 			}
-			agent2 := torneo.Agents[j]
+			agent2 := tournament.Agents[j]
 			wp, adp := tabla.Metrics(agent1.UID(), agent2.UID())
 			row = append(row, fmt.Sprintf("%.3f  %.3f", wp, adp))
 		}
@@ -112,32 +112,32 @@ func (torneo *Tournament) PrintWrTable(tabla Table) {
 	t.Render()
 }
 
-func (torneo *Tournament) PrintWaldTable(tabla Table) {
+func (tournament *Tournament) PrintWaldTable(tabla Table) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(
 		append(
 			table.Row{"A\\B"},
 			append(
-				torneo.Inscriptos("L    U"),
+				tournament.Inscriptos("L    U"),
 				"B/A",
 			)...,
 		),
 	)
 
-	for i := 0; i < len(torneo.Agents); i++ {
+	for i := 0; i < len(tournament.Agents); i++ {
 
-		agent1 := torneo.Agents[i]
+		agent1 := tournament.Agents[i]
 		row := []interface{}{
 			agent1.UID(),
 		}
 
-		for j := 0; j < len(torneo.Agents); j++ {
+		for j := 0; j < len(tournament.Agents); j++ {
 			if i == j {
 				row = append(row, "             ")
 				continue
 			}
-			agent2 := torneo.Agents[j]
+			agent2 := tournament.Agents[j]
 			// wp, adp := tabla.Metrics(agent1.UID(), agent2.UID())
 			// row = append(row, fmt.Sprintf("%.3f  %.3f", wp, adp))
 			u, l := tabla.WaldInterval(agent1.UID(), agent2.UID())
@@ -154,14 +154,14 @@ func (torneo *Tournament) PrintWaldTable(tabla Table) {
 	t.Render()
 }
 
-func (torneo *Tournament) Report() {
-	if torneo.NumDoubleGames > 0 {
-		log.Printf("%d Double games:\n", torneo.NumDoubleGames)
+func (tournament *Tournament) Report() {
+	if tournament.NumDoubleGames > 0 {
+		log.Printf("%d Double games:\n", tournament.NumDoubleGames)
 
 		log.Printf("TABLE: WR (Win Rate) & ADP (Avg. Diff. Points) for A vs B\n\n")
-		torneo.PrintWrTable(torneo.Table)
+		tournament.PrintWrTable(tournament.Table)
 
 		log.Printf("TABLE: Adjusted Wald Intervals (90%%) for A vs B\n\n")
-		torneo.PrintWaldTable(torneo.Table)
+		tournament.PrintWaldTable(tournament.Table)
 	}
 }
