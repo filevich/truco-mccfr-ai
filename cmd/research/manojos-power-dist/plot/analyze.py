479b2046e14fc306bbae5661682110011c206f05
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-02-04 21:30:43 -0300

    ft: add cdf py impl to truco dist

--- cmd/research/manojos-power-dist/plot/analyze.py
+++ cmd/research/manojos-power-dist/plot/analyze.py
@@ -18,11 +18,25 @@ def analyse_continuity(data):
     print(f"are they continuous? {are_keys_continuous}")
     if not are_keys_continuous: print(f"no, these are missing: {not_in_data}")
 
+def total_sum(data):
+    return sum([v for v in data.values()])
+
+def cdf(data, key):
+    s = 0
+    for i in range(0, key +1):
+        s += data[i] if i in data else 0
+    t = total_sum(data)
+    return s / t
+
 print("analysing data_envido")
 analyse_continuity(data_envido)
+print(f"data_envido total sum: {total_sum(data_envido)}")
 
 print("analysing data_flor")
 analyse_continuity(data_flor)
+print(f"data_flor total sum: {total_sum(data_flor)}")
 
 print("analysing data_power")
 analyse_continuity(data_power)
+print(f"data_power total sum: {total_sum(data_power)}")
+print(f"data_power.cdf(84)={cdf(data_power, 84)}")
