299e09cdd027b76743aa2a040fb9685ddedebf06
J.P. Filevich <juan.filevich@fing.edu.uy>
2024-02-07 23:32:58 -0300

    chore: add pro to tiny

--- cmd/eval/tiny/main.go
+++ cmd/eval/tiny/main.go
@@ -15,7 +15,7 @@ func main() {
 	const (
 		tinyEval   = 1_000
 		numPlayers = 2
-		b          = "/Users/jp/Downloads/cluster/train-cfr/models/2p/a2"
+		b          = "/Users/jp/Downloads/cluster/train-cfr/models/2p"
 	)
 
 	log.Println("loading t1k22")
@@ -25,13 +25,23 @@ func main() {
 	testThese := []bot.Agent{
 		&cfr.BotCFR{
 			N: "esvmccfra2",
-			F: b + "/final_es-vmccfr_d70h0m_D70h0m_t7077536_p0_a2_2402052107.model",
+			F: b + "/a2/final_es-vmccfr_d70h0m_D70h0m_t7077536_p0_a2_2402052107.model",
+		},
+		&cfr.BotCFR{
+			N: "esvmccfra3",
+			F: b + "/a3/final_es-vmccfr_d70h0m_D70h0m_t3468734_p0_a3_2402052116.model",
 		},
 	}
 
 	againstThese := []bot.Agent{
 		&bot.Random{},
 		&bot.Simple{},
+		&bot.Pro{
+			Alphas: []float32{6, 4, 6},
+			Betas:  []float32{-1, 0, -0.9},
+			Gammas: []float32{2, 2, 2},
+			Ks:     []float32{0, 0, 0},
+		},
 	}
 
 	for i, agent := range testThese {
