c7f7b7d22e5af48d0988f58ccf7e6c7c0391b2e4
J.P. Filevich <juan.filevich@fing.edu.uy>
2025-11-05 12:24:41 -0300

    ref: rm deprecated files

--- cfr/utils.go
+++
@@ -1,7 +0,0 @@
-package cfr
-
-import "github.com/filevich/truco-mccfr-ai/utils"
-
-func counterfactualReachProbability(probs []float32, player int) float32 {
-	return utils.Prod(probs[:player]) * utils.Prod(probs[player+1:])
-}
