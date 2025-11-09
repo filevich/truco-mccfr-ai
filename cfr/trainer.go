647e30bfc777dc930d599f9f230229294a0e17e0
J.P. Filevich <juan.filevich@fing.edu.uy>
2025-11-08 22:00:22 -0300

    ft: include flag to select which algorithm the exploiter will run

--- cfr/trainer.go
+++ cfr/trainer.go
@@ -471,9 +471,12 @@ func Embed(t Trainer_T, base *Trainer) ITrainer {
 	// unimplemented yet:
 	// case OSMCCFR_T:
 	// 	return &OSMCCFR{base, 0.1} // epsilon
+
 	// exploitability
 	case BR_T:
 		return &BestResponse{base}
+	case BR_FULL_T:
+		return &BestResponseFull{base}
 	default:
 		panic("trainer unknown")
 	}
